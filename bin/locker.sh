ef35d0e282f757cc6e246ae36bfae3b3e5224d8e
Noah Lutz <noah.l98@gmail.com>
2016-01-27 11:44:22 -0500

    added comments and changed all file names to the same thing

--- bin/locker.sh
+++ bin/locker.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+#Defined paths
 LOCK_IMAGE=/home/noah/pictures/lock.png
 TMP_DIR=/tmp/screenlock
 
-
+#Checks if tempdir is created, if not it is created
 [ -d $TMP_DIR ] || mkdir -p $TMP_DIR
 
-scrot $TMP_DIR/screenshot.png
+#takes screenshot and places it in the temp dir
+scrot $TMP_DIR/lockscreen.png
 
-convert $TMP_DIR/screenshot.png -blur 3x3 $TMP_DIR/screenshot.png
+#Makes the image blurry
+convert $TMP_DIR/lockscreen.png -blur 3x3 $TMP_DIR/lockscreen.png
 
-composite -gravity center $LOCK_IMAGE $TMP_DIR/screenshot.png $TMP_DIR/lockscreen.png
+#Overlays lock image over center and saves it
+composite -gravity center $LOCK_IMAGE $TMP_DIR/lockscreen.png $TMP_DIR/lockscreen.png
 
+#Runs i3lock with this image
 i3lock -n -i $TMP_DIR/lockscreen.png
